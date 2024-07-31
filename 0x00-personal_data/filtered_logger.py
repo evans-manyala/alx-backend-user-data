00be35594cbbd76374c129dceceeed02c2c0eac2
Evans Manyala <evans.manyala@gmail.com>
2024-08-01 00:01:59 +0300

    Fixing errors in script

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,24 +1,16 @@
 #!/usr/bin/env python3
+"""
+Obfuscates specified fields in a log message.
+"""
 import re
 from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str,
-                 pattern: str, separator: str) -> str:
+                 message: str, separator: str) -> str:
     """
     Obfuscates specified fields in a log message.
-
-    Args:
-        fields (List[str]): List of field names to obfuscate.
-        redaction (str): String to replace field values with.
-        message (str): The log line to be processed.
-        separator (str): Character that separates
-        fields in the log line.
-
-    Returns:
-        str: The log line with specified fields obfuscated.
     """
-    for field in fields:
-        pattern = re.sub(f'{field}=.*?{separator}',
-                         f'{field}={redaction}{separator}', pattern)
-    return pattern
+    pattern = "|".join(f"{field}=.*?(?={separator}|$)" for field in fields)
+    return re.sub(pattern, lambda m:
+                  f"{m.group().split('=')[0]}={redaction}", message)

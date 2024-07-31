7d9a74d6e7a129c2c6d54acc9365f814e182edbb
Evans Manyala <evans.manyala@gmail.com>
2024-08-01 02:31:27 +0300

    Adding error fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,6 +7,7 @@ from typing import List
 import logging
 import os
 import mysql.connector
+from mysql.connector import Error
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
@@ -69,12 +70,27 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     Connects to the MySQL database using
     credentials from environment variables.
     """
-    user = os.getenv('PERSONAL_DATA_DB_USERNAME') or "root"
-    passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD') or ""
-    host = os.getenv('PERSONAL_DATA_DB_HOST') or "localhost"
+    user = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
     db_name = os.getenv('PERSONAL_DATA_DB_NAME')
-    conn = mysql.connector.connect(user=user,
-                                   password=passwd,
-                                   host=host,
-                                   database=db_name)
-    return conn
+    if not db_name:
+        raise ValueError(
+            "PERSONAL_DATA_DB_NAME environment variable is required.")
+
+    try:
+        conn = mysql.connector.connect(user=user,
+                                       password=passwd,
+                                       host=host,
+                                       database=db_name)
+        if conn.is_connected():
+            print("Successfully connected to the database")
+            return conn
+        else:
+            raise ConnectionError("Failed to connect to the database")
+    except Error as e:
+        print(f"Error: {e}")
+        raise
+    except ConnectionError as ce:
+        print(ce)
+        raise

d5a83e243f26caee26dc1d34e4313bc2ff5255f5
Evans Manyala <evans.manyala@gmail.com>
2024-08-08 00:32:46 +0300

    Ficing Pycodestyle errors

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,8 +4,10 @@ Definition of class SessionAuth
 """
 from .auth import Auth
 
+
 class SessionAuth(Auth):
     """
     Session-based authentication class that inherits from Auth.
     """
+
     pass

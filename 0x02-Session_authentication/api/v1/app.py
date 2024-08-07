9d77098e7979eacb095ab9bb769d5183ee916c37
evans-manyala <evans.manyala@gmail.com>
2024-08-07 23:16:13 +0300

    Adding .gitignore and SEssion Auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -12,7 +12,6 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-# Initialize authentication mechanism based on environment variable
 auth = None
 AUTH_TYPE = os.getenv("AUTH_TYPE")
 if AUTH_TYPE == "auth":
@@ -23,6 +22,10 @@ elif AUTH_TYPE == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
 
     auth = BasicAuth()
+elif AUTH_TYPE == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+
+    auth = SessionAuth()
 
 
 @app.before_request
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Definition of class SessionAuth
+"""
+from .auth import Auth
+
+class SessionAuth(Auth):
+    """
+    Session-based authentication class that inherits from Auth.
+    """
+    pass

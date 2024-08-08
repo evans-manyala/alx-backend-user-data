be6a771c3cf82b16638492dce077f3c713053bfe
Evans Manyala <evans.manyala@gmail.com>
2024-08-09 02:07:56 +0300

    Adding code logic

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -38,11 +38,17 @@ def before_request():
     if auth is None:
         pass
     else:
-        excluded = ["/api/v1/status/", "/api/v1/unauthorized/",
-                    "/api/v1/forbidden/"]
+        setattr(request, "current_user", auth.current_user(request))
+        excluded = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
+        ]
         if auth.require_auth(request.path, excluded):
-            if auth.authorization_header(request) is None:
-                abort(401, description="Unauthorized")
+            cookie = auth.session_cookie(request)
+            if auth.authorization_header(request) is None and cookie is None:
+               abort(401, description="Unauthorized")
             if auth.current_user(request) is None:
                 abort(403, description="Forbidden")
 

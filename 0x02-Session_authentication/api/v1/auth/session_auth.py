32691b47242ef158b0951153921f481de089abea
Evans Manyala <evans.manyala@gmail.com>
2024-08-10 14:04:53 +0300

    Adding script logic

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""
+Definition of class SessionAuth
+"""
+import base64
+from uuid import uuid4
+from typing import TypeVar, Optional
+
+from .auth import Auth
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """
+    Implements Session Authorization protocol methods,
+    managing user sessions.
+    """
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: Optional[str] = None) -> Optional[str]:
+        """
+        Creates a unique session ID for a user and associates
+        it with the given user ID.
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id:
+                               Optional[str] = None) -> Optional[str]:
+        """
+        Retrieves the user ID associated with a given session ID.
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> TypeVar("User"):
+        """
+        Returns the User instance based on the session ID
+        stored in a cookie.
+        """
+        session_cookie = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_cookie)
+        return User.get(user_id) if user_id else None
+
+    def destroy_session(self, request=None) -> bool:
+        """
+        Deletes a user's session by removing the session ID.
+        """
+        if request is None:
+            return False
+        session_cookie = self.session_cookie(request)
+        if session_cookie is None:
+            return False
+        user_id = self.user_id_for_session_id(session_cookie)
+        if user_id is None:
+            return False
+        del self.user_id_by_session_id[session_cookie]
+        return True

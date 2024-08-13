31b36caef956276f5470d072ea0c7b6f450629d3
Evans Manyala <evans.manyala@gmail.com>
2024-08-13 14:02:53 +0300

    Implementing Auth.register_user in the Auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,15 +3,36 @@
 Hashed Password Utility module
 """
 
+from db import DB
+from user import User
+from bcrypt import hashpw, gensalt
 
-import bcrypt
 
-
-def _hash_password(password: str) -> bytes:
+class Auth:
     """
-    Hashes a password using bcrypt
+    Authentication class
     """
 
-    salt = bcrypt.gensalt()
-    hashed_passwd = bcrypt.hashpw(password.encode("utf-8"), salt)
-    return hashed_passwd
+    def __init__(self) -> None:
+        self._db = DB()
+
+    def _hash_password(password: str) -> bytes:
+        """
+        Hashes a password using bcrypt
+        """
+
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
+        return hashed_password
+
+    def register_user(self, email, password):
+        """
+        Registers a new user with the given email and password
+        """
+        try:
+            existing_user = self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = self._hash_password(password)
+            new_user = self._db.add_user(email=email, hashed_password=hashed_password)
+            return new_user

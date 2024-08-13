6dd28eafef8bfd7fea339353343f35d86a658cf3
Evans Manyala <evans.manyala@gmail.com>
2024-08-13 14:17:40 +0300

    Registering user end-point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,6 +4,10 @@ Basic Flask App
 """
 
 from flask import Flask, jsonify
+from auth import Auth
+from flask import request
+
+AUTH = Auth()
 
 
 app = Flask(__name__)
@@ -17,6 +21,27 @@ def home():
         Response: A JSON response with a message
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def register_user():
+    """
+    Handle POST requests to the /users URL
+    Returns:
+        Response: A JSON response with a message
+    """
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not email or password:
+        return jsonify({"error": "Email and password are required"}), 400
+    try:
+        user = AUTH.register_user(email=email, password=password)
+        return jsonify({"email": user.email, "message":
+                        "User created successfully"})
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 400
 
 
 if __name__ == "__main__":

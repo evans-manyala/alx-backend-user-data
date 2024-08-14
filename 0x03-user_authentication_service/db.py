4f9274ca11295f6c8af30f64ccc06146da1db417
evans-manyala <evans.manyala@gmail.com>
2024-08-14 14:30:09 +0300

    Fixing errors in script

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,9 +6,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import NoResultFound, InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
-from user import Base
+from user import Base, User
 
 
 class DB:

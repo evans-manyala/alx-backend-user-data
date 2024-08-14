0a8713e5e0706e3f79885c5e560b8ca63e57400f
evans-manyala <evans.manyala@gmail.com>
2024-08-14 16:08:45 +0300

    Adding Auth class to interact with authentication database

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -16,3 +16,25 @@ def _hash_password(password: str) -> bytes:
 
     hashed_password = password.encode('utf-8')
     return bcrypt.hashpw(hashed_password, bcrypt.gensalt())
+
+
+class Auth:
+    """
+    Authentication class
+    """
+
+    def __init__(self) -> None:
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Registers a new user with the given email and password
+        """
+        try:
+            existing_user = self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = self._hash_password(password)
+            new_user = self._db.add_user(email=email,
+                                         hashed_password=hashed_password)
+            return new_user

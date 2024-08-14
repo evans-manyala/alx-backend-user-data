bccc461620003118915b40b2b06c95522af93cf0
evans-manyala <evans.manyala@gmail.com>
2024-08-14 15:24:00 +0300

    Defining method to generate string that will be salted into a hash

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -9,6 +9,16 @@ import bcrypt
 from sqlalchemy.orm.exc import NoResultFound
 
 
+def _hash_password(self, password: str) -> bytes:
+    """
+    Hashes a password using bcrypt
+    """
+
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
+    return hashed_password
+
+
 class Auth:
     """
     Authentication class
@@ -16,15 +26,6 @@ class Auth:
 
     def __init__(self) -> None:
         self._db = DB()
-
-    def _hash_password(self, password: str) -> bytes:
-        """
-        Hashes a password using bcrypt
-        """
-
-        salt = bcrypt.gensalt()
-        hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
-        return hashed_password
 
     def register_user(self, email: str, password: str) -> User:
         """

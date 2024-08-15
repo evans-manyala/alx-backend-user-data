66ca0b623028bd8906951241619051bf0c71e8b9
Evans Manyala <evans.manyala@gmail.com>
2024-08-16 01:52:02 +0300

    Fixing errors validating user login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -42,10 +42,19 @@ class Auth:
     def valid_login(self, email: str, password: str) -> bool:
         """
         Validates a user's login credentials
+
+        Args:
+            email (str): user's email address
+            password (str): user's password
+        Return:
+            True if credentials are correct, else False
         """
+
         try:
             user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(password.encode('utf-8'),
-                                  user.hashed_password)
         except NoResultFound:
             return False
+
+        user_password = user.hashed_password
+        passwd = password.encode("utf-8")
+        return bcrypt.checkpw(passwd, user_password)

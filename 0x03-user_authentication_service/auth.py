e1e71475d62a995975d90707080760730d2f8f0b
Evans Manyala <evans.manyala@gmail.com>
2024-08-16 02:45:13 +0300

    Fixing errors in creating user sesions

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -83,7 +83,8 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
             session_id = _generate_uuid()
-            self._db.update_user(user.id, session_id=session_id)
+            user.session_id = session_id
+            self._db.session.commit()
             return session_id
         except NoResultFound:
             raise ValueError("User not found")

940d3903dec945c63d4acd5a304663b03fa78bf6
Evans Manyala <evans.manyala@gmail.com>
2024-08-17 02:15:50 +0300

    Adding update_password method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -140,3 +140,23 @@ class Auth:
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
+    
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        Update the password of a user with the given reset token.
+
+        Args:
+            reset_token (str): The reset password token.
+            password (str): The new password.
+
+        Returns:
+            None
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError(f"{reset_token} is not a valid reset token.")
+
+        hashed_password = _hash_password(password).decode()
+        self._db.update_user(user.id, hashed_password=hashed_password,
+                             reset_token=None)

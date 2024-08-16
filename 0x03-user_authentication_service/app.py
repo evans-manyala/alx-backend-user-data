7ecf4d25c2ca94bd5351d4f490be06e05d483162
Evans Manyala <evans.manyala@gmail.com>
2024-08-16 03:04:17 +0300

    Fixing errors with user login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -55,27 +55,26 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
-@app.route('/sessions', methods=['POST'], strict_slashes=False)
-def login():
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
     """
-    Login a user and create a session.
+    Log in a user if the credentials provided are correct, and create a new
+    session for them.
     """
-    email = request.form.get('email')
-    password = request.form.get('password')
+    email = request.form.get("email")
+    password = request.form.get("password")
 
     if not AUTH.valid_login(email, password):
-        abort(401)  # Incorrect login information
+        abort(401)
 
     session_id = AUTH.create_session(email)
     if session_id is None:
-        abort(401)  # Failed to create a session
+        abort(401)
 
-    response = make_response(jsonify({
-        "email": email,
-        "message": "session created"
-    }))
-    response.set_cookie('session_id', session_id)
-    return response
+    resp = jsonify({"email": email, "message": "logged in"})
+    resp.set_cookie("session_id", session_id, secure=True,
+                    httponly=True, samesite='Lax')
+    return resp
 
 
 if __name__ == "__main__":

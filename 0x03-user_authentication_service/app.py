28711d2337aaf5a5c2b69e2b81879a47045173ff
Evans Manyala <evans.manyala@gmail.com>
2024-08-16 12:29:53 +0300

    Fixing errors

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -26,3 +26,30 @@ def index():
         Response: A JSON response with a welcome message.
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """
+    Handle POST requests to the /users endpoint to register a new user.
+
+    Returns:
+        Response: A JSON response indicating the result of the user
+        registration.
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    # Validate input
+    if not email or not password:
+        return jsonify({"message": "email and password are required"}), 400
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 201
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
